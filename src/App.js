7c990ea2aad483517f5122197a1c605c49f74726
unknown <sonwj0915@naver.com>
2021-03-04 21:59:35 +0900

    props 사용법

--- src/App.js
+++ src/App.js
@@ -1,23 +1,25 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
 
+// function Food({ fav }) {
+//   return <h1>I like {fav}</h1>;
+// }
+
+function Food(props) {
+  return <h1>I like {props.fav}</h1>;
+}
+
+const foodILike = [];
+
+// name or fav(anything) = propname(property)
+// function App() 안에서 사용된 '<Food />컴포넌트'의 'property'를 function Food()의 argument(인자)로 넣어줌
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <h1>Hello!!!</h1>
+      <Food fav="kimchi" />
+      <Food fav="ramen" />
+      <Food fav="samgyopsal" />
+      <Food fav="chukumi" />
     </div>
   );
 }
