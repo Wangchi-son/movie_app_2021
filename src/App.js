a61b6942caff27d86200701fcf04e539a5b75d2b
unknown <sonwj0915@naver.com>
2021-03-04 23:21:29 +0900

    class컴포넌트와 state 기본 사용법

--- src/App.js
+++ src/App.js
@@ -1,70 +1,20 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 
-// function Food({ fav }) {
-//   return <h1>I like {fav}</h1>;
-// }
+// function 컴포넌트와 class 컴포넌트의 차이점은
+// function 컴포넌트는 function이고 무언가를 return함 그리고 screen에 표시됨
+// class 컴포넌트는 class이지만 react component로부터 확장되고 screen에 표시되고 그걸 render method 안에 넣어야 함
 
-function Food({ name, picture, rating }) {
-  return (
-    <div>
-      <h2>I like {name}</h2>
-      <h4>
-        <small>{rating}/5.0</small>
-      </h4>
-      <img src={picture} alt={name}></img>
-    </div>
-  );
-}
-
-Food.propTypes = {
-  name: PropTypes.string.isRequired,
-  picture: PropTypes.string.isRequired,
-  rating: PropTypes.number,
-};
-
-const foodILike = [
-  {
-    id: 1,
-    name: 'Kimchi',
-    image: 'http://aeriskitchen.com/wp-content/uploads/2008/09/kimchi_bokkeumbap_02-.jpg',
-  },
-  {
-    id: 2,
-    name: 'Samgyeopsal',
-    image:
-      'https://3.bp.blogspot.com/-hKwIBxIVcQw/WfsewX3fhJI/AAAAAAAAALk/yHxnxFXcfx4ZKSfHS_RQNKjw3bAC03AnACLcBGAs/s400/DSC07624.jpg',
-    rating: 4.9,
-  },
-  {
-    id: 3,
-    name: 'Bibimbap',
-    image:
-      'http://cdn-image.myrecipes.com/sites/default/files/styles/4_3_horizontal_-_1200x900/public/image/recipes/ck/12/03/bibimbop-ck-x.jpg?itok=RoXlp6Xb',
-    rating: 4.8,
-  },
-  {
-    id: 4,
-    name: 'Doncasu',
-    image: 'https://s3-media3.fl.yelpcdn.com/bphoto/7F9eTTQ_yxaWIRytAu5feA/ls.jpg',
-    rating: 5.5,
-  },
-  {
-    id: 5,
-    name: 'Kimbap',
-    image: 'http://cdn2.koreanbapsang.com/wp-content/uploads/2012/05/DSC_1238r-e1454170512295.jpg',
-    rating: 4.7,
-  },
-];
-
-function App() {
-  return (
-    <div>
-      {foodILike.map((dish) => (
-        <Food key={dish.id} name={dish.name} picture={dish.image} rating={dish.rating} />
-      ))}
-    </div>
-  );
+// App 컴포넌트는 React 컴포넌트를 확장시켜 가져옴
+// state = object
+// component의 data를 넣을 공간이 있고 state데이터는 변함 (바꿀 데이터를 state에 넣어야됨)
+class App extends React.Component {
+  state = {
+    count: 0,
+  };
+  render() {
+    return <h1>The number is {this.state.count}</h1>;
+  }
 }
 
 export default App;
