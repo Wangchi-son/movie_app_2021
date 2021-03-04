df14e0cd082297ea64fa3a24e5de918ad407679f
unknown <sonwj0915@naver.com>
2021-03-04 23:00:25 +0900

    PropTypes=한번더 체크하기 위한 용도

--- src/App.js
+++ src/App.js
@@ -1,58 +1,68 @@
-import React from "react";
+import React from 'react';
+import PropTypes from 'prop-types';
 
 // function Food({ fav }) {
 //   return <h1>I like {fav}</h1>;
 // }
 
-function Food({ name, picture }) {
+function Food({ name, picture, rating }) {
   return (
     <div>
       <h2>I like {name}</h2>
-      <img src={picture}></img>
+      <h4>
+        <small>{rating}/5.0</small>
+      </h4>
+      <img src={picture} alt={name}></img>
     </div>
   );
 }
 
+Food.propTypes = {
+  name: PropTypes.string.isRequired,
+  picture: PropTypes.string.isRequired,
+  rating: PropTypes.number,
+};
+
 const foodILike = [
   {
-    name: "Kimchi",
-    image:
-      "http://aeriskitchen.com/wp-content/uploads/2008/09/kimchi_bokkeumbap_02-.jpg",
+    id: 1,
+    name: 'Kimchi',
+    image: 'http://aeriskitchen.com/wp-content/uploads/2008/09/kimchi_bokkeumbap_02-.jpg',
   },
   {
-    name: "Samgyeopsal",
+    id: 2,
+    name: 'Samgyeopsal',
     image:
-      "https://3.bp.blogspot.com/-hKwIBxIVcQw/WfsewX3fhJI/AAAAAAAAALk/yHxnxFXcfx4ZKSfHS_RQNKjw3bAC03AnACLcBGAs/s400/DSC07624.jpg",
+      'https://3.bp.blogspot.com/-hKwIBxIVcQw/WfsewX3fhJI/AAAAAAAAALk/yHxnxFXcfx4ZKSfHS_RQNKjw3bAC03AnACLcBGAs/s400/DSC07624.jpg',
+    rating: 4.9,
   },
   {
-    name: "Bibimbap",
+    id: 3,
+    name: 'Bibimbap',
     image:
-      "http://cdn-image.myrecipes.com/sites/default/files/styles/4_3_horizontal_-_1200x900/public/image/recipes/ck/12/03/bibimbop-ck-x.jpg?itok=RoXlp6Xb",
+      'http://cdn-image.myrecipes.com/sites/default/files/styles/4_3_horizontal_-_1200x900/public/image/recipes/ck/12/03/bibimbop-ck-x.jpg?itok=RoXlp6Xb',
+    rating: 4.8,
   },
   {
-    name: "Doncasu",
-    image:
-      "https://s3-media3.fl.yelpcdn.com/bphoto/7F9eTTQ_yxaWIRytAu5feA/ls.jpg",
+    id: 4,
+    name: 'Doncasu',
+    image: 'https://s3-media3.fl.yelpcdn.com/bphoto/7F9eTTQ_yxaWIRytAu5feA/ls.jpg',
+    rating: 5.5,
   },
   {
-    name: "Kimbap",
-    image:
-      "http://cdn2.koreanbapsang.com/wp-content/uploads/2012/05/DSC_1238r-e1454170512295.jpg",
+    id: 5,
+    name: 'Kimbap',
+    image: 'http://cdn2.koreanbapsang.com/wp-content/uploads/2012/05/DSC_1238r-e1454170512295.jpg',
+    rating: 4.7,
   },
 ];
 
-function renderFood(dish) {
-  return <Food name={dish.name} picture={dish.image} />;
-}
-
-// name or fav(anything) = propname(property)
-// function App() 안에서 사용된 '<Food />컴포넌트'의 'property'를 function Food()의 argument(인자)로 넣어줌
-// map((dish)) <(dish)는 오브젝트(돌면서 순서대로 현재 처리 중)
 function App() {
   return (
     <div>
-      {console.log(foodILike.map(renderFood))}
-      {foodILike.map(renderFood)}
+      {foodILike.map((dish) => (
+        <Food key={dish.id} name={dish.name} picture={dish.image} rating={dish.rating} />
+      ))}
     </div>
   );
 }
