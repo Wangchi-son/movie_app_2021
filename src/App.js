78dc2634a879e60103ff5551face52e3605e5c5d
unknown <sonwj0915@naver.com>
2021-03-05 23:13:30 +0900

    Router Link  props 사용법

--- src/App.js
+++ src/App.js
@@ -1,8 +1,10 @@
 import React from 'react';
 import { HashRouter, Route } from 'react-router-dom';
+import './App.css';
 import Home from './routes/Home';
 import About from './routes/About';
 import Navigation from './components/Navigation';
+import Detail from './routes/Detail';
 
 function App() {
   return (
@@ -10,6 +12,7 @@ function App() {
       <Navigation />
       <Route path="/" exact={true} component={Home} />
       <Route path="/about" component={About} />
+      <Route path="/movie-detail" component={Detail} />
     </HashRouter>
   );
 }
