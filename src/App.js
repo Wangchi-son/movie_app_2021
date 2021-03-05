fffb2c286b54b52ccebff62c56eacbcf3e02f9fe
unknown <sonwj0915@naver.com>
2021-03-05 21:49:22 +0900

    기본 Movie.css, App.css 작업

--- src/App.js
+++ src/App.js
@@ -1,5 +1,7 @@
 import React from 'react';
 import axios from 'axios';
+import Movie from './Movie';
+import './App.css';
 
 class App extends React.Component {
   state = {
@@ -8,15 +10,42 @@ class App extends React.Component {
   };
   // async는 비동기 함수임을 알려주는 기능, await 요소가 끝날때까지 기다림
   getMovies = async () => {
-    const movies = await axios.get('https://yts.mx/api/v2/list_movies.json');
+    const {
+      data: {
+        data: { movies },
+      },
+    } = await axios.get('https://yts.mx/api/v2/list_movies.json?sort_by=rating');
+    this.setState({ movies, isLoading: false });
   };
   componentDidMount() {
     this.getMovies();
   }
 
   render() {
-    const { isLoading } = this.state;
-    return <div>{isLoading ? 'Loading' : 'We are ready'}</div>;
+    const { isLoading, movies } = this.state;
+    return (
+      <section className="container">
+        {isLoading ? (
+          <div className="loader">
+            <span className="loader__text">Loading...</span>
+          </div>
+        ) : (
+          <div className="movies">
+            {movies.map((movie) => (
+              <Movie
+                key={movie.id}
+                id={movie.id}
+                year={movie.year}
+                title={movie.title}
+                summary={movie.summary}
+                poster={movie.medium_cover_image}
+                genres={movie.genres}
+              />
+            ))}
+          </div>
+        )}
+      </section>
+    );
   }
 }
 
